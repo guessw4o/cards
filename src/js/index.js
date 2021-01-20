cebccaf222d4935e441c1621dd9728ba33485956
Andrew <catsgamingru@gmail.com>
2021-01-20 15:38:41 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
+const required = true
+const patientPurpose = { name: 'purpose', label: 'Цель визита', required }
+const patientDescription = { name: 'description', label: 'Краткое описание визита' }
+//срочность добавить
+const patientPressure = { name: 'pressure', label: 'Обычное давление', required }
+const patientBodyMass = { name: 'body-mass', label: 'Индекс массы тела', required }
+const patientCardiovascular = { name: 'cardiovascular', label: 'Перенесенные заболевания сердечно-сосудистой', required }
+const patientLastVisit = { name: 'last-visit', label: 'Дата последнего посещения', required }
+const patientAge = { name: 'age', label: 'Возраст', required }
+const patientFullName = { name: 'full-name', label: 'ФИО', required }
+
+const formFieldsByType = {
+    cardiologist: [
+        patientPurpose,
+        patientDescription,
+        patientPressure,
+        patientBodyMass,
+        patientCardiovascular,
+        patientAge,
+        patientFullName,
+    ],
+    dentist: [
+        patientPurpose,
+        patientDescription,
+        patientLastVisit,
+        patientFullName,
+    ],
+    therapist: [
+        patientPurpose,
+        patientDescription,
+        patientAge,
+        patientFullName,
+    ],
+}
+console.log('234')
+
+function handleDoctor(e) {
+    console.log(e.target)
+    const { value } = e.target
+    const formFields = formFieldsByType[value]
+    formFields.forEach(({ name, label, required }) => {
+        console.log(name)
+    })
+}
+
+// const modalDoctor = document.querySelector(".modal-doctor");
+
+// const rowFormDoctor = document.createElement('div');
+// rowFormDoctor.className = "row form-doctor";
+// modalDoctor.append(rowFormDoctor)
+
+// const inputFieldColS11 = document.createElement('div');
+// inputFieldColS11.className = "input-field col s11";
+// rowFormDoctor.append(inputFieldColS11)
+
+// const input = document.createElement("input");
+// input.id = "text3";
+// input.type = "text";
+// inputFieldColS11.append(input)
+
+// const label = document.createElement("label");
+// label.for = "text3";
+// label.innerHTML = "бла бла js";
+// inputFieldColS11.append(label)
+
+
---
+++ src/js/views/header.js
@@ -0,0 +1,26 @@
+const Header = document.createElement('header')
+Header.className = "header hoverable";
+document.body.prepend(Header)
+
+const LogoA = document.createElement('a')
+LogoA.href = "#"
+const LogoImg = document.createElement('img')
+LogoImg.src = "src/img/logo2.png"
+LogoImg.alt = "Logo"
+Header.append(LogoA)
+LogoA.append(LogoImg)
+
+const HeaderBtn = document.createElement('div')
+Header.append(HeaderBtn)
+
+const EnterBtn = document.createElement('a')
+EnterBtn.className = "waves-effect waves-light btn-large modal-trigger"
+EnterBtn.href = "#signin"
+EnterBtn.innerHTML = "Вход"
+HeaderBtn.append(EnterBtn)
+
+const CreateBtn = document.createElement('a')
+CreateBtn.className = "waves-effect waves-light btn-large modal-trigger"
+CreateBtn.href = "#create"
+CreateBtn.innerHTML = "Создать"
+HeaderBtn.append(CreateBtn)
