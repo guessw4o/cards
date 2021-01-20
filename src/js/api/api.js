ec5593702509480f96d3195bbee0a2c558deddb3
Andrew <catsgamingru@gmail.com>
2021-01-20 19:57:48 +0200

    header on js

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -3,6 +3,11 @@ const EnterSite = document.querySelector(".enter-btn")
 const InputEmail = document.querySelector("#email")
 const InputPassword = document.querySelector("#password")
 
+const EnterError = document.querySelector(".enter-error")
+
+let HeaderBtn__enter = document.querySelector(".headerBtn__enter")
+let HeaderBtn__create = document.querySelector(".headerBtn__create")
+
 EnterSite.onclick=function() {
     fetch("https://ajax.test-danit.com/api/cards/login", {
         method: "post",
@@ -14,10 +19,26 @@ EnterSite.onclick=function() {
         })
     })
         .then((response) => {
+            if (response.status !== 200) {
+                EnterError.innerHTML="Incorrect Login or Password"
+            }else{
+                HeaderBtn__enter.classList.remove("active");
+                HeaderBtn__create.classList.add("active");
+            }
             return response.text();
         })
         .then((data) => {
-            console.log(data);
             localStorage.setItem('token', data)
         })
+    
+}
+
+const token = localStorage.getItem("token")
+
+if (token===null) {
+    HeaderBtn__enter.classList.add("active");
+    HeaderBtn__create.classList.remove("active");
+}else{
+    HeaderBtn__enter.classList.remove("active");
+    HeaderBtn__create.classList.add("active");
 }
