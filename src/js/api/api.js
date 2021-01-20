52a5e27eafb8c1aae92319e0dfe9f9bf08e4653f
Andrew <catsgamingru@gmail.com>
2021-01-20 18:50:35 +0200

    enter api

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -3,19 +3,21 @@ const EnterSite = document.querySelector(".enter-btn")
 const InputEmail = document.querySelector("#email")
 const InputPassword = document.querySelector("#password")
 
-EnterSite.onclick=function(){
-        fetch("https://ajax.test-danit.com/api/cards/login", {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "email":InputEmail.value, "password":InputPassword.value
-            })
+EnterSite.onclick=function() {
+    fetch("https://ajax.test-danit.com/api/cards/login", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            "email": InputEmail.value, "password": InputPassword.value
         })
-            .then(response => response.text())
-            .catch(error => console.error('Error:', error))
-            .then(response => console.log(JSON.stringify(response)))
+    })
+        .then((response) => {
+            return response.text();
+        })
+        .then((data) => {
+            console.log(data);
+            localStorage.setItem('token', data)
+        })
 }
-
